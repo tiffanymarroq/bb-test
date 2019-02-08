f6f3be8dbcb0ccb11a776cda1c8058672a78d4b0
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-02-08 13:15:27 -0500

    added more styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log('hi');
+// Main text 
+function textStyle(){
+    document.getElementById("banner").style.background = 'rgb(230,230,250)';
+}
+
+//Button
+function button(){
+
+}
+
+//Main Function
+
+function main(){
+    textStyle();
+    
+}
+
+main();
