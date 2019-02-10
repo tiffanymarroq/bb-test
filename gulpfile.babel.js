c53af828a07b33e04c743f2f7ab0bd37a2bdfd71
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-02-10 10:26:57 -0500

    changed bg color controsl

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,16 +17,7 @@ var paths = {
 var autoprefixerOptions = {
   browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
 };
-// gulp.task('opt-img', () => {
-//   gulp.src('assets/images/*')
-//   .pipe(imagemin({
-//     interlaced: true,
-//     progressive: true,
-//     optimizationLevel: 7,
-//     svgoPlugins: [{removeViewBox: true}]
-// }, {verbose: false}))
-//   .pipe(gulp.dest('dist/images/'))
-// });
+
 export function connectServer(){
     connect.server({
         root:'dist',
@@ -42,30 +33,23 @@ export function reload() {
 
 export function styleBundle() {
   return gulp.src('styles/main.css')
-    .pipe(sourcemaps.init())
-    // .pipe(sass().on('error', sass.logError))
+
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cssnano())
-    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/styles/'))
 
 }
 
 export function scriptBundle() {
   return gulp.src('./scripts/main.js')
-        .pipe(concat('main.min.js'))
-        // .pipe(gulp.dest('dist'))
-        // .pipe(rename('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts/'));
+    .pipe(concat('main.min.js'))
 
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'));
 }
-
-
-
 
 export function watch() {
     gulp.watch(paths.scripts, scriptBundle);
--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,44 @@
-// Main text 
+// Colors
+var BLUE = "#5DBCD2" ;
+var RED = "#A21F1E";
+var YELLOW = "#F1AB3E";
+
+// Main text Animation
 function textStyle(){
     // document.getElementById("banner").style.background = 'rgb(0,0,0)';
     // document.getElementById("banner").style.backgroundImage = "url('assets/images/blue.jpg')";
 }
 
-//Button
-function button(){
+//Color Controls
+function buttonControls(){
     var colorBtn = document.getElementsByClassName("color-box");
-    var bgImage = document.getElementById("banner").style.backgroundImage;
-       colorBtn.onclick = function(){
-        console.log('in')
-        if(colorBtn.classList.contains("red")){
-            bgImage =  "url('assets/images/red.jpg')";
-
-        }else if (colorBtn.classList.contains("yellow")){
-            bgImage =  "url('assets/images/yellow.jpg')";
-            
-        }else{
-            bgImage =  "url('assets/images/blue.jpg')";
-        }
-    }
-
+    var bannerContainer = document.getElementById("banner-container")
+    var bgColor = BLUE;
+    bannerContainer.classList.add("frame-padding");
+    document.getElementById("banner").style.backgroundImage = "url('assets/images/blue.jpg')";
+    colorBtn[0].addEventListener("click", function(){
+        document.getElementById("banner").style.backgroundImage =  "url('assets/images/red.jpg')";      
+        bgColor = RED;
+        bannerContainer.style.backgroundColor = bgColor;
+    });
+    colorBtn[1].addEventListener("click", function(){
+        document.getElementById("banner").style.backgroundImage =  "url('assets/images/yellow.jpg')";
+        bgColor = YELLOW;
+        bannerContainer.style.backgroundColor = bgColor;
+    });
+    colorBtn[2].addEventListener("click", function(){
+        document.getElementById("banner").style.backgroundImage =  "url('assets/images/blue.jpg')";      
+        bgColor = BLUE;
+        bannerContainer.style.backgroundColor = bgColor;
+    });
+    bannerContainer.style.backgroundColor = bgColor;
 }
 
 //Main Function
 
 function main(){
     textStyle();
-
-    button();
-    var colorBtn = document.getElementsByClassName("color-box");
-    document.getElementById("banner").style.backgroundImage = "url('assets/images/blue.jpg')";
-    
-    console.log(colorBtn[0])
-    colorBtn[0].addEventListener("click", function(){
-        document.getElementById("banner").style.backgroundImage =  "url('assets/images/red.jpg')";        
-      });
-
-      colorBtn[1].addEventListener("click", function(){
-        document.getElementById("banner").style.backgroundImage =  "url('assets/images/yellow.jpg')";        
-      });
-      colorBtn[2].addEventListener("click", function(){
-        document.getElementById("banner").style.backgroundImage =  "url('assets/images/blue.jpg')";        
-      });
-
-    
+    this.buttonControls();
 }
 
 main();
